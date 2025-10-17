b8b2fd544050d2d9b13106ef5088ae23064154f1
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-10-17 13:14:07 +0200

    feat(auth): implement endpoint to verify jwt

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,8 @@ const express = require("express")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+
 const User = require("../models/User.model");
 
 const router = express.Router();
@@ -133,6 +135,20 @@ router.post('/login', (req, res, next) => {
     });
 });
 
+
+
+//
+// GET /auth/verify (Verify a JWT)
+//
+router.get('/verify', isAuthenticated, (req, res, next) => {
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  console.log(`req.payload`, req.payload);
+ 
+  // Send back the object with user data
+  // previously set as the token payload
+  res.json(req.payload);
+});
 
 
 
